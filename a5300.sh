da209fa7343a4fd7033996f70d025086c8e7cbb1
wuxianlin <wuxianlinwxl@gmail.com>
2014-03-22 11:26:55 +0800

    add example device

---
+++ a5300.sh
@@ -0,0 +1,2 @@
+./deodex.sh ./app ./framework 17 core.jar:core-junit.jar:bouncycastle.jar:ext.jar:framework.jar:telephony-common.jar:mms-common.jar:android.policy.jar:services.jar:apache-xml.jar:mediatek-common.jar:mediatek-framework.jar:mediatek-op.jar:secondary-framework.jar:CustomProperties.jar
+
---
+++ r801.sh
@@ -0,0 +1,2 @@
+./deodex.sh ./app ./framework 10 core.jar:bouncycastle.jar:ext.jar:framework.jar:oppo.framework.jar:oppo.services.jar:android.policy.jar:services.jar:core-junit.jar:mtklockscreen.jar
+
