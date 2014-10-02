0db27726eea430008080074d9ad4f8329554b18e
wuxianlin <wuxianlinwxl@gmail.com>
2014-10-02 12:27:47 +0800

    delete example device

--- a5300.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call deodex 17 core.jar:core-junit.jar:bouncycastle.jar:ext.jar:framework.jar:telephony-common.jar:mms-common.jar:android.policy.jar:services.jar:apache-xml.jar:mediatek-common.jar:mediatek-framework.jar:mediatek-op.jar:secondary-framework.jar:CustomProperties.jar
-exit
--- r801.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call deodex 10 core.jar:bouncycastle.jar:ext.jar:framework.jar:oppo.framework.jar:oppo.services.jar:android.policy.jar:services.jar:core-junit.jar:mtklockscreen.jar
-exit
